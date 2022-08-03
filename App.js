5d16614a19dd6f7adff59d55a77f644e8c50f4fd
Lee MinWoo <dlalsdn1009@naver.com>
2022-08-03 22:29:54 +0900

    Update bottom tap

--- App.js
+++ App.js
@@ -1,49 +1,115 @@
 import * as React from 'react';
 import {Component} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
-    StyleSheet,
-    View,
-    Text,
-    Button,
+  StyleSheet,
+  View,
+  Text,
+  Button,
 } from 'react-native';
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 
-const HomeScreen = ({ navigation, route }) => {
-    return (
-      <View style={styles.container}>
-        <Text>홈 페이지</Text>
-        <Button
-          title="네비게이션 btn"
-          onPress={() =>
-              navigation.navigate('Profile')
-          }/>
-      </View>
+// 네비게이션 설정
+const Setting = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "#EEE" }]}>
+      <Text> 세팅 페이지</Text>
+    </View>
+  );
+};
+const Home = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "#EEE" }]}>
+      <Text> 홈 페이지</Text>
+    </View>
+  );
+};
+const Chat = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "#EEE" }]}>
+      <Text> 채팅 페이지</Text>
+    </View>
+  );
+};
+const MyPage = () => {
+  return (
+    <View style={[styles.container, { backgroundColor: "#EEE" }]}>
+      <Text> 마이 페이지</Text>
+    </View>
   );
 };
 
-const StudyScreen = ({ navigation, route }) => {
-    // const [postText, setPostText] = React.useState('');
-    return (
-      <View style={styles.profile}>
-        <Text>스터디 상세 스크린</Text>
-      </View>
-      )
-};
+
+// 네이게이션 최상단 설정
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Root"
+        component={TabNavigator}
+        options={ {headerShown: false} }
+        />
+    </Stack.Navigator>
+  );
+}
+// 하단바 네비게이션
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="홈"
+      activeColor= "#56D3DB"
+      barStyle={{ backgroundColor: "white", border: "none" }}>
+
+      <Tab.Screen name="설정" component={Setting} 
+        options={{headerShown: false,
+          tabBarLabel: 'Setting',
+          tabBarIcon: ({ color }) => (
+          <Ionicons name="ios-settings" color={color} size={25} />
+        ),
+      }} />
+      <Tab.Screen name="홈" component={Home} 
+        options={{headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+          <Ionicons name="home" color={color} size={25} />
+          ),
+        }} />
+      <Tab.Screen name="대화" component={Chat}
+        options={{headerShown: false,
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({ color }) => (
+          <Ionicons name="chatbox" color={color} size={25} />
+        ),
+      }} />
+      <Tab.Screen name="마이페이지" component={MyPage}
+        options={{headerShown: false,
+            tabBarLabel: 'MyPage',
+            tabBarIcon: ({ color }) => (
+            <Ionicons name="person" color={color} size={25} />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
-  return (
+    return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} initialParams={{name:'root'}}  options={{ headerShown: false }} />
-          <Stack.Screen name="Profile" component={StudyScreen} />
-        </Stack.Navigator>
+        <RootNavigator />
       </NavigationContainer>
-  );
-};
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -51,11 +117,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  profile: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    }
 });
 
 export default App;
