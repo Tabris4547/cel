674137137164932e9c2ef1b58a1999e36babde84
Kook Hye Gyeong <locker50@naver.com>
2022-08-12 02:18:34 +0900

    update index.js

--- server/k0000k/index.js
+++ server/k0000k/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`${port}에서 서버 실행중`);
+})
