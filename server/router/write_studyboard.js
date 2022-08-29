0a88c97c1e606448d67d463b96f32127cc41d1b3
kkg4547@naver.com <kkg4547@naver.com>
2022-08-29 16:47:32 +0900

    post_revise

--- server/router/write_studyboard.js
+++ server/router/write_studyboard.js
@@ -9,11 +9,24 @@ app post('/write',(req,res))=>{
   const mento_presence=req.body.mento_presence;
   const study_president_id=req.body.study_president_id;
   const open_chat_link=req.body.open_chat_link;
+  const study_tag=req.body.study_tag;
+  const num = 0;
 
-  const sqlQuery="INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)"+
+  const sqlQuery1="INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)"+
             " values(?,?,?,?,?,now(),?)";
-  db.query(sqlQuery,(err,result)=>{
-    res.redirect('/')
+
+
+  db.query('SELECT study_number from study where study_name=study_name',(err,result))=>{
+    num = result[0].study_number;
+  })
+  const sqlQuery2="INSERT into study_tag(study_number,study_tag)"+
+                  "values(num,?)";
+  db.query(sqlQuery1,(err,result)=>{
+
+  })
+
+  db.query(sqlQuery2,(err,result)=>{
+    req.redirect('/')
   })
 }
 module.exports = router;
