a639ca589676cab42ddec8910b16f207667e95c8
Tabris4547 <99371497+Tabris4547@users.noreply.github.com>
2022-09-07 22:36:10 +0900

    revise_needtest

--- server/router/studyboardPost.js
+++ server/router/studyboardPost.js
@@ -20,22 +20,40 @@ router.post('/', (req, res) => {
 
     const sqlQuery1 = "INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)" +
         " values(?,?,?,?,?,now(),?);"
-    const sqlQuery2 = "SELECT study_number from study where study_name=test;"
-    const sqlQuery3 = "INSERT into study_tag(num,study_tag)"+"values(?,?);"
+    // const sqlQuery2 = "SELECT study_number from study where study_name='test';"
+    const sqlQuery2="SELECT study_number INTO $num FROM study WHERE study_name='test'"
+                  +"CREATE PROCEDURE loopInsert()"
+                  +"BeGIN"
+                  +"Declare i INT DEFAULT 0"
+                  +"where i<?.length"
+                  +"DO"
+                  +"INSERT into study_tag(num,study_tag)"+"values($num,?.[i])"
+                  +"SET i=i+1"
+                  +"END while"
+                  +"END"
+                  +"CALL loopInsert();"
+    // const sqlQuery3 = "INSERT into study_tag(num,study_tag)"+"values(?,?);"
 
     db.query(sqlQuery1,values, (err, result) => {
 
     })
-
-    db.query(sqlQuery2,study_name, (err, result) => {
-        num = result;
-        console.log(study_name)
-        console.log(result)
-
-    })
-
-
-    db.query(sqlQuery3,num,tags,(err, result) => {
+    // for(int i=0;i<tags.length;i++){
+    //   db.query(sqlQuery2,tags[i], (err, result) => {
+    //       console.log(tags[i])
+    //       res.redirect('/')
+    //
+    //     })
+    //   }
+    db.query(sqlQuery2,tags, (err, result) => {
+        console.log(tags.length)
         res.redirect('/')
+
     })
+
+    //
+    // db.query(sqlQuery3,num,tags,(err, result) => {
+    //     res.redirect('/')
+    // })
+
+})
 module.exports = router;
