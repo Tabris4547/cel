505b4545735aaca3380dd9eaaba5a3030e6b1ad3
kkg4547@naver.com <kkg4547@naver.com>
2022-09-07 18:28:37 +0900

    revise

--- server/router/write_studyboard.js
+++ server/router/write_studyboard.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../dbconnect");
-//스터디 정보 적기
-app post('/write',(req,res))=>{
-  const study_name=req.body.study_name;
-  const explanation=req.body.explanation;
-  const rule=req.body.rule;
-  const mento_presence=req.body.mento_presence;
-  const study_president_id=req.body.study_president_id;
-  const open_chat_link=req.body.open_chat_link;
-  const study_tag=req.body.study_tag;
-  const num = 0;
-
-  const sqlQuery1="INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)"+
-            " values(?,?,?,?,?,now(),?)";
-  db.query(sqlQuery1,(err,result)=>{
-
-  })
-  db.query('SELECT study_number from study where study_name=study_name',(err,result))=>{
-    num = result[0].study_number;
-  })
-  const sqlQuery2="INSERT into study_tag(study_number,study_tag)"+
-                  "values(num,?)";
 
 
-  db.query(sqlQuery2,(err,result)=>{
-    req.redirect('/')
-  })
-}
+router.post('/', (req, res) => {
+
+    const study_name = req.body.study_name;
+    const explanation = req.body.explanation;
+    const rule = req.body.rule;
+    const mento_presence = req.body.mento_presence;
+    const study_president_id = req.body.study_president_id;
+    const open_chat_link = req.body.open_chat_link;
+    const study_tags = req.body.study_tags;
+
+    var values=[study_name,explanation,rule,mento_presence,study_president_id,open_chat_link]
+    var tags=study_tags.split(',')
+
+    let num = 0;
+
+    const sqlQuery1 = "INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)" +
+        " values(?,?,?,?,?,now(),?);"
+    const sqlQuery2 = "SELECT study_number from study where study_name=test;"
+    const sqlQuery3 = "INSERT into study_tag(num,study_tag)"+"values(?,?);"
+
+    db.query(sqlQuery1,values, (err, result) => {
+
+    })
+
+    db.query(sqlQuery2,study_name, (err, result) => {
+        num = result[0];
+        console.log(study_name)
+        console.log(result[0])
+
+    })
+
+
+    db.query(sqlQuery3,num,tags,(err, result) => {
+        res.redirect('/')
+    })
+
+
+})
+
 module.exports = router;
