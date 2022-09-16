2fdeb644ba4af2778ab12944e115f39b5b51e63e
kkg4547@naver.com <kkg4547@naver.com>
2022-09-16 14:41:53 +0900

    revise_write

--- server/router/write_studyboard.js
+++ server/router/write_studyboard.js
@@ -16,30 +16,70 @@ router.post('/', (req, res) => {
     var values=[study_name,explanation,rule,mento_presence,study_president_id,open_chat_link]
     var tags=study_tags.split(',')
 
-    let num = 0;
+    var num = 2;
 
     const sqlQuery1 = "INSERT into study(study_name,explanation,rule,mento_presence,study_president_id,study_start_date,open_chat_link)" +
         " values(?,?,?,?,?,now(),?);"
-    const sqlQuery2 = "SELECT study_number from study where study_name=test;"
-    const sqlQuery3 = "INSERT into study_tag(num,study_tag)"+"values(?,?);"
+    const sqlQuery2="SELECT study_number FROM study WHERE study_name=?;"
+    const sqlQuery3="INSERT into study_tag(study_number,study_tag) values(?,?);"
+
+    // const sqlQuery2 = "SELECT study_number from study where study_name='test';"
+    // const sqlQuery2="SELECT study_number INTO @num FROM study WHERE study_name='test'"
+    //               +"CREATE PROCEDURE loopInsert()"
+    //               +"BeGIN"
+    //               +"Declare i INT DEFAULT 0"
+    //               +"where i<?.length"
+    //               +"DO"
+    //               +"INSERT into study_tag(study_number,study_tag)"+"values(@num,?.[i])"
+    //               +"SET i=i+1"
+    //               +"END while"
+    //               +"END"
+    //               +"CALL loopInsert();"
+    // const sqlQuery3 = "INSERT into study_tag(num,study_tag)"+"values(?,?);"
 
     db.query(sqlQuery1,values, (err, result) => {
-
+    })
+    db.query(sqlQuery2,study_name,(err,result)=>{
+        num=result[0].study_number
+        console.log(result);
+        console.log(num);
+        db.query(sqlQuery3,[num,tags[0]],(err,result)=>{
+          console.log(num);
+        })
+        db.query(sqlQuery3,[num,tags[1]],(err,result)=>{
+          console.log(num);
+        })
+        db.query(sqlQuery3,[num,tags[2]],(err,result)=>{
+          console.log(num);
+          res.redirect('/')
+        })
     })
 
-    db.query(sqlQuery2,study_name, (err, result) => {
-        num = result[0];
-        console.log(study_name)
-        console.log(result[0])
 
-    })
+    // for(var i=0;i<3;i++){
+    //   db.query(sqlQuery2,tags[i], (err, result) => {
+    //       console.log(tags);
+    //       console.log(tags[0]);
+    //       console.log(tags[1]);
+    //       console.log(tags[2]);
+    //       console.log(i)
+    //       console.log(tags[i])
+    //
+    // 이 방식대로 하면 i=3으로 찍히네요....
+    //     })
+    //   }
+    // db.query(sqlQuery2,tags, (err, result) => {
+    //     console.log(tags.length)
+    //     console.log(tags[0])
+    //     console.log('ok')
+    //     res.redirect('/')
+    //
+    // })
 
-
-    db.query(sqlQuery3,num,tags,(err, result) => {
-        res.redirect('/')
-    })
-
+    //
+    // db.query(sqlQuery3,num,tags,(err, result) => {
+    //     res.redirect('/')
+    // })
 
 })
-
 module.exports = router;
