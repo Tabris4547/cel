07ea2fcbdade17c2fdfb7ec7a63dc058bc0f7c5a
Tabris4547 <99371497+Tabris4547@users.noreply.github.com>
2022-09-19 18:05:15 +0900

    revise

--- server/router/detailGet.js
+++ server/router/detailGet.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../dbconnect");
 
-router.get('/detail/get/:number', (req, res) => {
+router.get('/', (req, res) => {
     const sqlQuery = "select * from study"+
         "where study_number=number;"
     db.query(sqlQuery, (err, result) => {
